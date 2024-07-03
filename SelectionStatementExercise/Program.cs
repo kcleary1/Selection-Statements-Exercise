51189dc5ed8eda0a274dd4642078afe534077ce3
Kyle Cleary <kscleary11@gmail.com>
2024-07-02 20:06:58 -0600

    Exercise Complete

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var favNumber = 22;
+            Console.WriteLine("Guess my favorite number");
+            var userInput = int.Parse(Console.ReadLine());
+            if (userInput < favNumber) { Console.WriteLine("Good guess, but too low."); }
+            else if (userInput > favNumber) { Console.WriteLine("Sorry, too high."); }
+            else if (userInput == favNumber) { Console.WriteLine("You guessed it!"); }
         }
     }
 }
